27f0b207ec84486c02044d414b4e214f64e5feb2
expmax85 <expmax85@gmail.com>
2022-07-01 18:25:15 +0500

    add to readme install giude

--- test_app/views.py
+++ test_app/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import ListView
 
 from test_app.models import Orders
-from test_app.tasks import poll_update
 
 
 class IndexView(ListView):
@@ -11,5 +10,4 @@ class IndexView(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(IndexView, self).get_context_data(*args, **kwargs)
-        poll_update()
         return context
